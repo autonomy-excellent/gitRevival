5d17dc870e6c126a8e320c23549045a93c2e412f
Stephen-java <71622272+autonomy-excellent@users.noreply.github.com>
2023-01-06 22:15:36 +0800

    forth commit

---
+++ src/main/java/com/atguigu/git/TestGit.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * @author wystart
+ * @create 2023-01-06 22:12
+ */
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
+        System.out.println("hello git4!");
+    }
+
+}
